bdd4c7e2210b8d72931cda6eb76ea2916acc0c32
ld-web <lucas@ld-web.net>
2026-07-28 16:54:09 +0200

    feat: article tags

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Entity\Tag;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -27,6 +28,15 @@ class ArticleType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'name',
             ])
+            ->add(
+                'tags',
+                EntityType::class,
+                [
+                    'class' => Tag::class,
+                    'choice_label' => 'label',
+                    'multiple' => true
+                ]
+            )
             ->add('Enregistrer', SubmitType::class)
         ;
     }
